825e7de9176010744dd9095b10a9e704385dd08a
Igor Diachuk <dabsonwork2@gmail.com>
2022-11-26 03:25:43 +0300

    if, switch case lesson

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
 "use sctrict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDb = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+// const personalMovieDb = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
 
 
-const
-  a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = prompt('На сколько оцените его?', ''),
-  c = prompt('Один из последних просмотренных фильмов?', ''),
-  d = prompt('На сколько оцените его?', '');
+// const
+//   a = prompt('Один из последних просмотренных фильмов?', ''),
+//   b = prompt('На сколько оцените его?', ''),
+//   c = prompt('Один из последних просмотренных фильмов?', ''),
+//   d = prompt('На сколько оцените его?', '');
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+// personalMovieDb.movies[a] = b;
+// personalMovieDb.movies[c] = d;
 
-console.log(personalMovieDb);
+// console.log(personalMovieDb);
+
+const num = '51';
+
+switch (num) {
+  case 49:
+    console.log('Неверно');
+    break;
+  case '51':
+    console.log('Верно');
+    break;
+  default:
+    console.log('Не угадал, в следующий раз тебе повезёт');
+    break;
+}
