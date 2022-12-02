1fe071dc3deeaa96705aef10eccc6aa63ef48cc6
Igor Diachuk <dabsonwork2@gmail.com>
2022-12-02 03:46:59 +0300

    another commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt('Сколько фильмом вы уже посмотрели?', ' ');
 
-while (num <= 55) {
-  console.log(num);
-  num++;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const
+        a = prompt('Один из последних просмотренных фильмов?', ' '),
+        b = prompt('На сколько оцените его?', ' ');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
 
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов', '');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель', '');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман', '');
+} else {
+    console.log('Произошла ошибка', '');
+}
+
+console.log(personalMovieDB);
